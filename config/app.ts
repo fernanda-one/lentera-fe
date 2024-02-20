e2292ffb062de5d7d571c6584c6adeff7fa483cf
anangsyah <anang.haqim@ist.id>
2024-02-20 23:29:41 +0700

    chore: add config base url

---
+++ config/app.ts
@@ -0,0 +1,8 @@
+export const appConfig = {
+  baseUrl:
+    (typeof window !== "undefined"
+      ? (window as any).serverEnv?.DYNAMIC_ENV_BASE_URL
+      : "") ||
+    process.env.NEXT_PUBLIC_BASE_URL ||
+    "https://official-joke-api.appspot.com",
+};
