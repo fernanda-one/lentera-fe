2203c3020efe358b42e1a5ee0f8f72088d97f009
anangsyah <anang.haqim@ist.id>
2024-03-21 13:49:32 +0700

    add: add detail category and category page

---
+++ app/(blog)/berita/[slug]/[id]/page.tsx
@@ -0,0 +1,16 @@
+import DetailCategoryBlog from '@/components/layouts/blog/DetailCategoryBlog'
+import React from 'react'
+
+const NewsDetail = () => {
+  return (
+    <DetailCategoryBlog
+      title={'Kajian Aswaja Kitab Firqah Annajiyah LTM MWCNU Adipala'}
+      imageUrl={'/blog-image1.png'}
+      creator={'Anang syah'}
+      date={'Minggu, 28 Nov 2021 01:22 WIB'}
+      description={'test saja'}
+    />
+  )
+}
+
+export default NewsDetail
--- app/(blog)/berita/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Berita = () => {
-  return (
-    <div className='h-screen'>Berita</div>
-  )
-}
-
-export default Berita
